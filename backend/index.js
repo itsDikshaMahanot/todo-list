c37048ab7d15ca96abd1fda2c47f7b61763dfa62
itsDikshaMahanot <jainmani786@gmail.com>
2024-02-25 17:16:50 +0530

    post and get metyhod added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,43 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { Todo } = require("./model/todo_schema");
 const app = express();
 const port = 3000;
+const mongooseURI =
+  "mongodb+srv://mani:mani123@cluster0.kw63si7.mongodb.net/todos";
+
+app.use(express.json());
+
+app.post("/todo", async (req, res) => {
+  console.log(req.body);
+  const { title, description, isCompleted } = req.body;
+
+  var todo = new Todo({
+    title: title,
+    description: description,
+    iscompleted: isCompleted,
+  });
+  await todo.save();
+  res.send({
+    title: title,
+    description: description,
+    iscompleted: isCompleted,
+  });
+});
+
+app.get("/todo", async (req, res) => {
+  Todo.find()
+    .then((todo) => res.send(todo))
+    .catch((error) => res.send("Error" + error));
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`);
+  await mongoose.connect(mongooseURI).then(() => {
+    console.log("DB Connected");
+  });
 });
